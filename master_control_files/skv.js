85169656f00670005418b66dce903bf7fd708239
Sasha Kovar <sasha-git@arcocene.org>
2015-08-27 15:58:21 -0700

    try sorting keys in HSL (effectively by hue)

--- master_control_files/skv.js
+++ master_control_files/skv.js
@@ -49,15 +49,72 @@ function offsetToKey(img, offset) {
   var g = img.data[offset + 1] & 0xff;
   var b = img.data[offset + 2] & 0xff;
   var a = img.data[offset + 3] & 0xff;
-  return r << 24 | g << 16 | b << 8 | a;
+
+  var hsl = rgbToHsl(r, g, b);
+
+  //return r << 24 | g << 16 | b << 8 | a;
+  return hsl[0] << 24 | hsl[1] << 16 | hsl[2] << 8 | a;
 }
 
 function keyToColor(key) {
+  // (really hsl)
   var r = (key & 0xFF000000) >>> 24;
   var g = (key & 0x00FF0000) >>> 16;
   var b = (key & 0x0000FF00) >>> 8;
   var a = (key & 0x000000FF);
-  return [r,g,b,a];
+
+  var rgb = hslToRgb(r,g,b);
+
+  return [rgb[0],rgb[1],rgb[2],a];
+}
+
+// thanks http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+    case g: h = (b - r) / d + 2; break;
+    case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+
+  return [h * 255, s * 255, l * 255];
+}
+
+function hslToRgb(h, s, l) {
+  h /= 255, s /= 255, l /= 255;
+
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p, q, t) {
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1/6) return p + (q - p) * 6 * t;
+      if(t < 1/2) return q;
+      if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    };
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
 function setColor(img, x, y, colr) {
